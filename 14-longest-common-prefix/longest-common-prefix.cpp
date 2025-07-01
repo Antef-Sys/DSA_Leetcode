36fcdf107748190f84f750b19b9fc854b349a2ea
Antef <antefidrisi@gmail.com>
2025-07-01 17:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (33.31%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,15 +3,24 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
 
-        for(int i=0;i<strs[0].length();++i){
-            char ch = strs[0][i];
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last=strs.back();
 
-            for(int j=1;j<strs.size();++j){
-                if(i>=strs[j].size() || strs[j][i] != ch){
-                    return strs[0].substr(0,i);
-                }
-            }
+        int i=0;
+        while(i < first.size() && first[i] == last[i]){
+            i++;
         }
-        return strs[0];
+
+        // for(int i=0;i<strs[0].length();++i){
+        //     char ch = strs[0][i];
+
+        //     for(int j=1;j<strs.size();++j){
+        //         if(i>=strs[j].size() || strs[j][i] != ch){
+        //             return strs[0].substr(0,i);
+        //         }
+        //     }
+        // }
+        return first.substr(0,i);
     }
 };
