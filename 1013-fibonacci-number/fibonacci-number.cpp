a611473e99ae7d3c6043a3462973baed47955635
Antef <antefidrisi@gmail.com>
2025-07-01 14:22:38 +0530

    Time: 2 ms (77.46%) | Memory: 7.8 MB (63.99%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,8 +4,15 @@ public:
         if(n==0)return 0;
 
         if(n==1)return 1;
+        int a=0,b=1;
+        int ans=0;
 
-        
-        return fib(n-1)+fib(n-2);
+        for(int i=2;i<=n;i++){
+            ans=a+b;
+            a=b;
+            b=ans;
+        }
+        return ans;
+        // return fib(n-1)+fib(n-2);
     }
 };
