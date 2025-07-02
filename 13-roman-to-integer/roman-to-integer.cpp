d346609160ff7caf458d1b7eb7b01d12f70154d0
Antef <antefidrisi@gmail.com>
2025-07-02 17:58:51 +0530

    Time: 8 ms (40.61%) | Memory: 13.4 MB (19.08%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -10,6 +10,17 @@ public:
             {'D',500},
             {'M',1000},
         };
+        // char ch;
+        // switch(ch){
+        //     case 'I':return 1;
+        //     case 'V':return 5;
+        //     case 'X':return 10;
+        //     case 'L':return 50;
+        //     case 'C':return 100;
+        //     case 'D':return 500;
+        //     case 'M':return 1000;
+        //     default:return 0;
+        // }
         int ans=0;
         for(int i=0; i<s.size();++i){
             if(m[s[i]] < m[s[i+1]])
