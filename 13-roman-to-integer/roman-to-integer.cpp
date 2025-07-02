fc9b189394744a2eeb0e620482d41d2d83906802
Antef <antefidrisi@gmail.com>
2025-07-02 18:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (84.68%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
+ int char2num(char ch){
+            switch(ch){
+            case 'I':return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D':return 500;
+            case 'M':return 1000;
+            default:return 0;
+            }
+        }
     int romanToInt(string s) {
-        unordered_map<char,int>m{
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000},
-        };
-        // char ch;
-        // switch(ch){
-        //     case 'I':return 1;
-        //     case 'V':return 5;
-        //     case 'X':return 10;
-        //     case 'L':return 50;
-        //     case 'C':return 100;
-        //     case 'D':return 500;
-        //     case 'M':return 1000;
-        //     default:return 0;
-        // }
+        // unordered_map<char,int>m{
+        //     {'I',1},
+        //     {'V',5},
+        //     {'X',10},
+        //     {'L',50},
+        //     {'C',100},
+        //     {'D',500},
+        //     {'M',1000},
+        // };
+        
         int ans=0;
         for(int i=0; i<s.size();++i){
-            if(m[s[i]] < m[s[i+1]])
-            ans=ans-m[s[i]];
+            if(char2num(s[i]) < char2num(s[i+1]))
+            ans=ans-char2num(s[i]);
 
             else
-            ans=ans+m[s[i]];
+            ans=ans+char2num(s[i]);
         }
         return ans;
     }
