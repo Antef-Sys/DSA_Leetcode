84ef1c97ef714d253fbfb54508cf0252cb2971ec
Antef <antefidrisi@gmail.com>
2025-09-02 17:37:53 +0530

    Time: 4 ms (56.95%) | Memory: 14.8 MB (50.47%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            // We start the inner loop from i + 1 to avoid using the same element twice
-            // and to avoid checking the same pair in reverse.
-            for (int j = i + 1; j < nums.size(); j++) {
-                // Check if the sum of the two elements equals the target.
-                if (nums[i] + nums[j] == target) {
-                    // If a solution is found, return a vector containing the two indices.
-                    return {i, j};
-                }
+        unordered_map<int,int> map;  
+        for(int i = 0; i < nums.size(); i++) {
+            int second = target - nums[i];
+            
+            if(map.count(second)) {
+                return {map[second], i};
             }
+            
+            map[nums[i]] = i;
         }
-        return {}; 
+        return {};  
+
+        
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         for (int j = i + 1; j < nums.size(); j++) {
+    //             if (nums[i] + nums[j] == target) {
+    //                 return {i, j};
+    //             }
+    //         }
+    //     }
+    //     return {}; 
+
+    
     }
 };
