1bdf6ceb302e57125150c1049c265707fdc0d3b1
Antef <antefidrisi@gmail.com>
2026-03-07 07:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.99%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int i=0;
+        if(s.length()==1) return false;
+        while(i<s.length()){
+            
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+
+                if(st.top()=='(' && s[i]==')' || 
+                   st.top()=='[' && s[i]==']' || 
+                   st.top()=='{' && s[i]=='}'){
+                    st.pop();
+                   }
+                
+                else{
+                    return false;
+                }
+            }
+           
+            i++;
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
