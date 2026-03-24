04aa787825b855d87d38d6cd3803baab9fddf58f
Antef <antefidrisi@gmail.com>
2026-03-24 19:30:57 +0530

    Time: 3 ms (47.06%) | Memory: 9.5 MB (91.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())return false;
-        map<char,int>mp;
+        // map<char,int>mp;
 
-        for(char ch : s)mp[ch]++;
-        for(char ch : t)mp[ch]--;
+        // for(char ch : s)mp[ch]++;
+        // for(char ch : t)mp[ch]--;
 
-        for(auto it : mp){
-            if(it.second != 0) return false;
-        }
-        return true;
+        // for(auto it : mp){
+        //     if(it.second != 0) return false;
+        // }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s==t)
+         return true;
+
+        else
+        return false; 
 
         
 
